fa1b570687658bc28bf1d97fe5541c6ea5af1bfd
Serhii Salatin <lifenjoy8888@gmail.com>
2023-07-26 22:23:26 +0300

    in ScraperService collectData method was impl

--- src/main/java/com/example/scraper/ScraperService.java
+++ src/main/java/com/example/scraper/ScraperService.java
@@ -1,6 +1,16 @@
 package com.example.scraper;
 
+import com.example.scraper.model.Item;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +20,63 @@ public class ScraperService {
     private String jobSiteUrl;
 
     public void collectData(List<String> categories) {
+        Document document;
+        try {
+            document = Jsoup.connect(jobSiteUrl).get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
+        Elements jobElements = document.select(".sc-beqWaB.gupdsY.job-card[data-testid=job-list-item]");
+
+        for (Element element: jobElements) {
+            var item = parseElementToItem(element);
+            //todo: save item to DB using ItemService
+        }
+    }
+
+    private Item parseElementToItem(Element jobElement) {
+        var jobPageUrl = "https://jobs.techstars.com"
+                + jobElement.selectFirst("a[data-testid=job-title-link]").attr("href");
+        var positionName = jobElement.selectFirst("div[itemprop=title]").text();
+        var organizationUrl = "https://jobs.techstars.com"
+                + jobElement.selectFirst("a[data-testid=link]").attr("href");
+        var logoUrl = jobElement.selectFirst("meta[itemprop=logo]").attr("content");
+        var organizationTitle = jobElement.selectFirst("meta[itemprop=name]").attr("content");
+        var laborFunction = jobElement.selectFirst("div[data-testid=tag]").text();
+        var location = jobElement.selectFirst("div[itemprop=jobLocation]").text();
+        var postedDate = parseDateToTimestamp(jobElement);
+        var description = jobElement.selectFirst("meta[itemprop=description]").attr("content");
+        var tags = parseTagsToSet(jobElement);
+
+        return new Item(
+                jobPageUrl,
+                positionName,
+                organizationUrl,
+                logoUrl,
+                organizationTitle,
+                laborFunction,
+                location,
+                postedDate,
+                description,
+                tags
+        );
+    }
+
+    private long parseDateToTimestamp(Element element) {
+        var postedDateStr = element.selectFirst("meta[itemprop=datePosted]").attr("content");
+        LocalDate localDate = LocalDate.parse(postedDateStr);
+        return localDate.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli();
+    }
+
+    private Set<String> parseTagsToSet(Element element) {
+        Elements tagElements = element.select("div[data-testid=tag]");
+
+        Set<String> tags = new HashSet<>();
+        for (Element tagElement : tagElements) {
+            tags.add(tagElement.text());
+        }
+
+        return tags;
     }
 }
--- src/main/java/com/example/scraper/model/Item.java
+++ src/main/java/com/example/scraper/model/Item.java
@@ -7,17 +7,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.Set;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "items")
 @Data
+@NoArgsConstructor
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String jobPageUrl;
     private String positionName;
-    private String urlToOrganization;
+    private String organizationUrl;
     private String logoUrl;
     private String organizationTitle;
     private String laborFunction;
@@ -25,4 +27,27 @@ public class Item {
     private Long timestamp;
     private String description;
     private Set<String> tags;
+
+    public Item(String jobPageUrl,
+                String positionName,
+                String organizationUrl,
+                String logoUrl,
+                String organizationTitle,
+                String laborFunction,
+                String location,
+                Long timestamp,
+                String description,
+                Set<String> tags) {
+
+        this.jobPageUrl = jobPageUrl;
+        this.positionName = positionName;
+        this.organizationUrl = organizationUrl;
+        this.logoUrl = logoUrl;
+        this.organizationTitle = organizationTitle;
+        this.laborFunction = laborFunction;
+        this.location = location;
+        this.timestamp = timestamp;
+        this.description = description;
+        this.tags = tags;
+    }
 }
