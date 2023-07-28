0ebec4368554d7e89ce21670fbb0562119721420
Serhii Salatin <lifenjoy8888@gmail.com>
2023-07-28 09:14:38 +0300

    removed redundant field from JobSearchController

--- src/main/java/com/example/scraper/controller/JobSearchController.java
+++ src/main/java/com/example/scraper/controller/JobSearchController.java
@@ -1,7 +1,6 @@
 package com.example.scraper.controller;
 
 import com.example.scraper.model.dto.ItemSearchResponseDto;
-import com.example.scraper.repository.ItemRepository;
 import com.example.scraper.service.SearchJobService;
 import com.example.scraper.service.mapper.ItemMapper;
 import java.util.List;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobSearchController {
     private final SearchJobService searchJobService;
     private final ItemMapper itemMapper;
-    private final ItemRepository repository;
 
     @GetMapping("/search")
     public ResponseEntity<Page<ItemSearchResponseDto>> search(
